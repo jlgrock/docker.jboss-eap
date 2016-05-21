d76ea5430d7a9b3644687bbc8bf7171fef7c7af3
Justin Grant <jlgrock@gmail.com>
2016-05-21 11:55:19 -0400

    fixing problem with vars build file

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@
 # load the versions
 . ./loadenv.sh
 
+FROM_IMAGE_NAME=jlgrock/centos-oraclejdk
+FROM_IMAGE_VERSION=6.6-8u45
+IMAGE_NAME=jlgrock/jboss-eap
+IMAGE_VERSION=$JBOSS_EAP
+TMP_IMAGE_NAME="${IMAGE_NAME}-temp"
+
 echo "Processing for JBOSS EAP Version $JBOSS_EAP"
 
 if [ ! -e install_files/jboss-eap-$JBOSS_EAP.zip ]; then
@@ -26,10 +32,23 @@ if [ ! -e install_files/activemq-rar*.rar ]; then
 	echo "You should put the required JBoss A-MQ connector in the root directory first."
 	exit 255
 fi
-# Create containers
-echo "Creating JBoss EAP Container ..."
-docker pull jlgrock/centos-oraclejdk:6.6-8u45
-docker build -q --rm -t jlgrock/jboss-eap:$JBOSS_EAP .
+
+# pull latest from repo
+docker pull ${FROM_IMAGE_NAME}:${FROM_IMAGE_VERSION}
+
+# Create a temporary image
+echo "Creating JBoss EAP Image ..."
+docker build -q --rm -t ${TMP_IMAGE_NAME}:${IMAGE_VERSION} .
+
+# Start Image and get ID
+ID=$(docker run -d ${TMP_IMAGE_NAME}:${IMAGE_VERSION} /bin/bash)
+echo "Container ID '$ID' now running"
+
+# Flatten the image (removes AUFS layers) and create a new image
+docker export ${ID} | docker import - ${IMAGE_NAME}
+echo "destroying images/containers related to $TMP_IMAGE_NAME (all versions)"
+docker ps -a | awk '{ print $1,$2 }' | grep ${TMP_IMAGE_NAME} | awk '{ print $1 }' | xargs -I {} docker rm {}
+docker images -a | awk '{ print $1, $3 }' | grep ${TMP_IMAGE_NAME} | awk '{ print $2 }' | xargs -I {} docker rmi {}
 
 if [ $? -eq 0 ]; then
     echo "Container Built"
